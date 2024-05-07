ec31a1b83b47b4ca37e9ddf508b763482da0d17d
biatrisz <132359853+biatrisz@users.noreply.github.com>
2024-05-07 16:40:02 -0300

    item

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,25 @@
-import { Text, View } from 'react-native';
-import { styles } from '../css/style';
+import {FlatList, View, Text} from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const data = [
+  { id: '1', title: 'Item 1'},
+  {id: '2', title: 'Item 2'},
+  {id: '3', title: 'Item 3'},
+  {id: '4', title: 'Item 4'},
+  {id: '5', title: 'Item 5'},
+];
+export default function listaSimples(){
+  const renderItem = ({item}) => (
+    <View style= {{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc'}}>
+      <Text>{item.title}</Text>
     </View>
   );
+  return(
+    <FlatList
+    horizontal
+    data = {data}
+    renderItem={renderItem}
+    keyExtractor={(item) => item.id}
+    />
+  );
 }
-
